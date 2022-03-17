7069a3a52662bb2f1e17cf3eb3cc8edf11375d94
kaussany <kaussany@sheridancollege.ca>
2022-03-17 17:16:04 -0400

    V4-Fix: implement role-based access control

--- code/deletearticle.php
+++ code/deletearticle.php
@@ -4,13 +4,13 @@ include("lib/auth.php");
 
 //check if current user is admin, if so, delete the article
 $aid = $_GET['aid'];
-if ($_SESSION['username'] === 'admin'){
+if ($_SESSION['role'] === 'admin'){
 	$result = delete_article($dbconn, $aid);
 	header("Location: /admin.php");
 }
 
 //check if current non-admin user is the author of the article, if yes, delete article
-if ($_SESSION['username'] !== 'admin'){
+if ($_SESSION['role'] !== 'admin'){
 	$result = get_article($dbconn, $aid);
 	$row = pg_fetch_array($result);
 	
--- code/editarticle.php
+++ code/editarticle.php
@@ -4,8 +4,26 @@
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
 	$aid = $_GET['aid'];	
+	
+	//if the current user's role is admin, access any article for edit
+	if ($_SESSION['role'] === 'admin'){
 	$result=get_article($dbconn, $aid);
 	$row = pg_fetch_array($result, 0);
+	}
+
+	//check if current user's role is not admin and if it is the author of the article. If yes, access article for edit
+	if ($_SESSION['role'] !== 'admin'){
+		$result = get_article($dbconn, $aid);
+		$row = pg_fetch_array($result);
+	
+		if($_SESSION['username'] === $row['author']) {
+			$row = pg_fetch_array($result, 0);	
+		} else {
+			header("Location: /admin.php");
+			exit();
+		}	
+	} 
+			
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$title = $_POST['title'];
 	$content = $_POST['content'];
@@ -28,14 +46,14 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <h2>New Post</h2>
 
 <form action='#' method='POST'>
-	<input type="hidden" value="<?php echo $row['aid'] ?>" name="aid">
+	<input type="hidden" value="<?php echo htmlspecialchars($row['aid']) ?>" name="aid">
 	<div class="form-group">
 	<label for="inputTitle" class="sr-only">Post Title</label>
-	<input type="text" id="inputTitle" required autofocus name='title' value="<?php echo $row['title'] ?>">
+	<input type="text" id="inputTitle" required autofocus name='title' value="<?php echo htmlspecialchars($row['title']) ?>">
 	</div>
 	<div class="form-group">
 	<label for="inputContent" class="sr-only">Post Content</label>
-	<textarea name='content' id="inputContent"><?php echo $row['content'] ?></textarea>
+	<textarea name='content' id="inputContent"><?php echo htmlspecialchars($row['content']) ?></textarea>
 	</div>
 	<input type="submit" value="Update" name="submit" class="btn btn-primary">
 </form>
